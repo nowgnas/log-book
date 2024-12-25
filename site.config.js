b1550c920ffe6ce369c4a02ef989e2cc68192f8e
nowgnas <dev.nowgnas@gmail.com>
2024-12-25 16:55:48 +0900

    change revalidate time and add seo keywords

--- site.config.js
+++ site.config.js
@@ -24,11 +24,13 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  link: "https://log-book-eight.vercel.app/",
+  since: 2024, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
-
+  seo: {
+    keywords: ["msa", "architecture", "blog"]
+  }
   // notion configuration (required)
   notionConfig: {
     pageId: process.env.NOTION_PAGE_ID || "167c50d6b07980e0ba48ebe9a0ac672e",
@@ -69,7 +71,7 @@ const CONFIG = {
     },
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
-  revalidateTime: 21600 * 7, // revalidate time for [slug], index
+  revalidateTime: 36000, // revalidate time for [slug], index
 }
 
 module.exports = { CONFIG }
